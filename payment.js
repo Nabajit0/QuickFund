c23218673b579c64c2e4cb69b80b77a5eaad0505
NABAJIT TALUKDAR <99857987+Nabajit0@users.noreply.github.com>
2026-06-12 19:39:49 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,1 +1,30 @@
 
+document.getElementById("payName").innerText =
+localStorage.getItem("name");
+
+document.getElementById("payAmount").innerText =
+localStorage.getItem("amount");
+
+function payNow() {
+
+const name = localStorage.getItem("name");
+const amount = localStorage.getItem("amount");
+
+const upiLink =
+    "upi://pay?pa=nabajittalukdar000-1@oksbi" +
+    "&pn=QuickFund" +
+    "&tn=" + encodeURIComponent(name) +
+    "&am=" + amount +
+    "&cu=INR";
+
+window.location.href = upiLink;
+
+setTimeout(() => {
+    window.location.href = "thankyou.html";
+}, 3000);
+
+}
+
+function comingSoon() {
+alert("Coming Soon!\n\nCurrently only UPI payment is available.");
+}
