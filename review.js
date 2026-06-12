6d49cc59d50806269d2b0512263a8ee86ee793c9
NABAJIT TALUKDAR <99857987+Nabajit0@users.noreply.github.com>
2026-06-12 19:33:48 +0530

    Update review.js

--- review.js
+++ review.js
@@ -1,1 +1,20 @@
 
+document.getElementById("showName").innerText =
+localStorage.getItem("name");
+
+document.getElementById("showMobile").innerText =
+localStorage.getItem("mobile");
+
+document.getElementById("showPurpose").innerText =
+localStorage.getItem("purpose");
+
+document.getElementById("showAmount").innerText =
+localStorage.getItem("amount");
+
+function editDetails(){
+window.location.href = "details.html";
+}
+
+function goPayment(){
+window.location.href = "payment.html";
+}
