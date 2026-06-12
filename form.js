294ca63fe75b29ba13b46f41ef9edd8d5c30f845
NABAJIT TALUKDAR <99857987+Nabajit0@users.noreply.github.com>
2026-06-12 17:40:05 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,1 +1,20 @@
+function saveDetails(){
 
+const name = document.getElementById("name").value;
+const mobile = document.getElementById("mobile").value;
+const purpose = document.getElementById("purpose").value;
+const amount = document.getElementById("amount").value;
+
+if(name === "" || mobile === "" || amount === ""){
+    alert("Please fill all required fields");
+    return;
+}
+
+localStorage.setItem("name", name);
+localStorage.setItem("mobile", mobile);
+localStorage.setItem("purpose", purpose);
+localStorage.setItem("amount", amount);
+
+window.location.href = "review.html";
+
+}
